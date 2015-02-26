875500d34de363f480078041ce394d54d3934ea0
Evan Moore <etmoore@gmail.com>
2015-02-26 10:16:19 -0700

    form and controller action for createRant

---
+++ app/controllers/rants.js
@@ -0,0 +1,15 @@
+import Ember from 'ember';
+
+export default Ember.ArrayController.extend({
+  actions: {
+    createRant: function() {
+      var title = this.get('title');
+      var body = this.get('body');
+      var newRant = this.store.createRecord('rant', { title: title, body: body });
+      newRant.save();
+
+      this.set('title', null);
+      this.set('body', null);
+    },
+  }
+});
